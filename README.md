447174f42215ac0d8275620e6e52d7b908df5940
AliSaim <Ali@Alis-MacBook-Air.local>
2017-03-09 17:18:43 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ali saim comp308-assignment2
+
+## Authentication with Passport - Assignment 2 for Comp308
+
+please use **`npm install`** to install project dependencies
---
+++ server/app.js
@@ -0,0 +1,65 @@
+let express = require('express');
+let path = require('path'); // part of node.js core
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+// import "mongoose" NPM Module
+let mongoose = require('mongoose');
+
+// import the config module
+let config = require('./config/db');
+
+// connect to the Mongo db using the URI above
+mongoose.connect(process.env.URI || config.URI);
+
+// create a db object and make a reference to the connection
+let db = mongoose.connection;
+
+// listen for a successful connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("Conneced to MongoDB...");
+});
+
+let index = require('./routes/index');
+let games = require('./routes/games');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client')));
+
+// route redirects
+app.use('/', index); // wildcard
+app.use('/games', games);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) =>{
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
